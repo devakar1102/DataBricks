41717c1ffd082377a7dfc5241f24e80e920806e9
devakar1102 <76725643+devakar1102@users.noreply.github.com>
2026-05-25 05:56:12 +0100

    Create DatabricksConfig.cs

---
+++ DatabricksSqlApp/Models/DatabricksConfig.cs
@@ -0,0 +1,8 @@
+namespace DatabricksSqlApp.Models;
+
+public class DatabricksConfig
+{
+    public string Host { get; set; } = string.Empty;
+    public string HttpPath { get; set; } = string.Empty;
+    public string AccessToken { get; set; } = string.Empty;
+}
